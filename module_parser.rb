7d841c74368e1e1ffb1e3f1d67fdb97bd4216f6b
maradudin <maradudindenis@gmail.com>
2024-02-20 13:01:11 +0300

    Build fixes

--- module_parser.rb
+++ module_parser.rb
@@ -4,8 +4,14 @@ local_folder = "headers"
 simulator_suffix = "simulator"
 device_suffix = "device"
 
-def process_contents(contents, suffix)
+def light_process_contents(contents, suffix)
     contents = contents.gsub(/\#include <libxml\/(.*?)\.h>/, "#import \"\\1-#{suffix}.h\"")
+
+    return contents
+end
+
+def process_contents(contents, suffix)
+    contents = light_process_contents(contents, suffix)
     contents = contents.gsub(/\#include <(.*?)\.h>/, '@import Darwin.C.\1;')
 
     return contents
@@ -18,12 +24,17 @@ def process_folder(headers_folder, local_folder, suffix)
         if fname == "." or fname == ".." then next end
 
         if fname == "DOCBparser.h" then next end #deprecated file
+        if fname == "module.modulemap" then next end #not a header
         
         local = fname.sub(/\.h/, "-#{suffix}.h")
         imports += "    header \"#{local}\"\n"
 
         contents = File.read(File.join(headers_folder, fname))
-        contents = process_contents(contents, suffix)
+        if fname == "xmlversion.h" then #Don`t change library prefix by "Darwin.C"
+            contents = light_process_contents(contents, suffix)
+        else
+            contents = process_contents(contents, suffix)
+        end
 
         writer = File.open(File.join(local_folder, local), "w")
         writer.write(contents)
@@ -32,7 +43,7 @@ def process_folder(headers_folder, local_folder, suffix)
     return imports
 end
 
-Dir.mkdir(local_folder) unless File.exists?(local_folder)
+Dir.mkdir(local_folder) unless File.exist?(local_folder)
 
 puts "framework module Libxml2 {"
 puts
